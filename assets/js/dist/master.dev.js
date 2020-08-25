f7c46c9beb33ebdb0bf522cf3c2c22d9260e94d4
Oscar Perez Sabogal <ospersa@gmail.com>
2020-08-24 21:54:33 -0500

    Inclusión lógica en jQuery para mostrar textos desde Json

---
+++ assets/js/dist/master.dev.js
@@ -0,0 +1,98 @@
+"use strict";
+
+var obj = {
+  "groups": {
+    "economicos": {
+      "types": {
+        "comandoycontrol": {
+          "instruments": {
+            "plusvalia": {
+              "name": "Plusvalía",
+              "definition": "Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad. Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad.",
+              "requirements": "Requiere tener claras las zonas y los tipos de terrenos: de desarrollo, en la medida que se consoliden, o en renovación, según el POT.",
+              "when": "Con un POT definido, cuando ocurre el cambio o venta del predio.",
+              "which": "Se combina con instrumentos de gestión como los planes parciales, las UPR. Es fuente de financiación, dado que el municipio recibe directamente lo recaudado."
+            },
+            "impuestos": {
+              "name": "Impuestos",
+              "definition": "Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad. Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad.",
+              "requirements": "Requiere tener claras las zonas y los tipos de terrenos: de desarrollo, en la medida que se consoliden, o en renovación, según el POT.",
+              "when": "Con un POT definido, cuando ocurre el cambio o venta del predio.",
+              "which": "Se combina con instrumentos de gestión como los planes parciales, las UPR. Es fuente de financiación, dado que el municipio recibe directamente lo recaudado."
+            },
+            "bancodetierras": {
+              "name": "Banco de tierras",
+              "definition": "Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad. Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad.",
+              "requirements": "Requiere tener claras las zonas y los tipos de terrenos: de desarrollo, en la medida que se consoliden, o en renovación, según el POT.",
+              "when": "Con un POT definido, cuando ocurre el cambio o venta del predio.",
+              "which": "Se combina con instrumentos de gestión como los planes parciales, las UPR. Es fuente de financiación, dado que el municipio recibe directamente lo recaudado."
+            },
+            "tasa": {
+              "name": "Tasa",
+              "definition": "Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad. Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad.",
+              "requirements": "Requiere tener claras las zonas y los tipos de terrenos: de desarrollo, en la medida que se consoliden, o en renovación, según el POT.",
+              "when": "Con un POT definido, cuando ocurre el cambio o venta del predio.",
+              "which": "Se combina con instrumentos de gestión como los planes parciales, las UPR. Es fuente de financiación, dado que el municipio recibe directamente lo recaudado."
+            },
+            "multa": {
+              "name": "Multa",
+              "definition": "Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad. Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad.",
+              "requirements": "Requiere tener claras las zonas y los tipos de terrenos: de desarrollo, en la medida que se consoliden, o en renovación, según el POT.",
+              "when": "Con un POT definido, cuando ocurre el cambio o venta del predio.",
+              "which": "Se combina con instrumentos de gestión como los planes parciales, las UPR. Es fuente de financiación, dado que el municipio recibe directamente lo recaudado."
+            },
+            "peajes": {
+              "name": "Peajes",
+              "definition": "Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad. Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad.",
+              "requirements": "Requiere tener claras las zonas y los tipos de terrenos: de desarrollo, en la medida que se consoliden, o en renovación, según el POT.",
+              "when": "Con un POT definido, cuando ocurre el cambio o venta del predio.",
+              "which": "Se combina con instrumentos de gestión como los planes parciales, las UPR. Es fuente de financiación, dado que el municipio recibe directamente lo recaudado."
+            },
+            "tarifas": {
+              "name": "Tarifas",
+              "definition": "Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad. Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad.",
+              "requirements": "Requiere tener claras las zonas y los tipos de terrenos: de desarrollo, en la medida que se consoliden, o en renovación, según el POT.",
+              "when": "Con un POT definido, cuando ocurre el cambio o venta del predio.",
+              "which": "Se combina con instrumentos de gestión como los planes parciales, las UPR. Es fuente de financiación, dado que el municipio recibe directamente lo recaudado."
+            },
+            "licenciaypermisos": {
+              "name": "Licencia y Permisos",
+              "definition": "Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad. Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad.",
+              "requirements": "Requiere tener claras las zonas y los tipos de terrenos: de desarrollo, en la medida que se consoliden, o en renovación, según el POT.",
+              "when": "Con un POT definido, cuando ocurre el cambio o venta del predio.",
+              "which": "Se combina con instrumentos de gestión como los planes parciales, las UPR. Es fuente de financiación, dado que el municipio recibe directamente lo recaudado."
+            },
+            "derechosdepropiedad": {
+              "name": "Derechos de Propiedad",
+              "definition": "Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad. Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad.",
+              "requirements": "Requiere tener claras las zonas y los tipos de terrenos: de desarrollo, en la medida que se consoliden, o en renovación, según el POT.",
+              "when": "Con un POT definido, cuando ocurre el cambio o venta del predio.",
+              "which": "Se combina con instrumentos de gestión como los planes parciales, las UPR. Es fuente de financiación, dado que el municipio recibe directamente lo recaudado."
+            }
+          }
+        }
+      }
+    }
+  }
+};
+$('document').ready(function () {
+  $('.instrument').click(function () {
+    var instrument = $(this).data("instrument");
+    var type = $(this).data("type");
+    var group = $(this).data("group");
+    var name = obj["groups"][group]["types"][type]["instruments"][instrument]["name"];
+    var definition = obj["groups"][group]["types"][type]["instruments"][instrument]["definition"];
+    var requirements = obj["groups"][group]["types"][type]["instruments"][instrument]["requirements"];
+    var when = obj["groups"][group]["types"][type]["instruments"][instrument]["when"];
+    var which = obj["groups"][group]["types"][type]["instruments"][instrument]["which"];
+    toogleTexts(name, definition, requirements, when, which);
+  });
+});
+
+function toogleTexts(name, definition, requirements, when, which) {
+  $('#instrument').html(name);
+  $('#definition').html(definition);
+  $('#requirements').html(requirements);
+  $('#when').html(when);
+  $('#which').html(which);
+}
---
+++ assets/js/master.js
@@ -0,0 +1,99 @@
+const obj = {
+    "groups": {
+        "economicos": {
+            "types": {
+                "comandoycontrol": {
+                    "instruments": {
+                        "plusvalia": {
+                            "name": "Plusvalía",
+                            "definition": "Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad. Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad.",
+                            "requirements": "Requiere tener claras las zonas y los tipos de terrenos: de desarrollo, en la medida que se consoliden, o en renovación, según el POT.",
+                            "when": "Con un POT definido, cuando ocurre el cambio o venta del predio.",
+                            "which": "Se combina con instrumentos de gestión como los planes parciales, las UPR. Es fuente de financiación, dado que el municipio recibe directamente lo recaudado."
+                        },
+                        "impuestos": {
+                            "name": "Impuestos",
+                            "definition": "Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad. Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad.",
+                            "requirements": "Requiere tener claras las zonas y los tipos de terrenos: de desarrollo, en la medida que se consoliden, o en renovación, según el POT.",
+                            "when": "Con un POT definido, cuando ocurre el cambio o venta del predio.",
+                            "which": "Se combina con instrumentos de gestión como los planes parciales, las UPR. Es fuente de financiación, dado que el municipio recibe directamente lo recaudado."
+                        },
+                        "bancodetierras": {
+                            "name": "Banco de tierras",
+                            "definition": "Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad. Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad.",
+                            "requirements": "Requiere tener claras las zonas y los tipos de terrenos: de desarrollo, en la medida que se consoliden, o en renovación, según el POT.",
+                            "when": "Con un POT definido, cuando ocurre el cambio o venta del predio.",
+                            "which": "Se combina con instrumentos de gestión como los planes parciales, las UPR. Es fuente de financiación, dado que el municipio recibe directamente lo recaudado."
+                        },
+                        "tasa": {
+                            "name": "Tasa",
+                            "definition": "Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad. Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad.",
+                            "requirements": "Requiere tener claras las zonas y los tipos de terrenos: de desarrollo, en la medida que se consoliden, o en renovación, según el POT.",
+                            "when": "Con un POT definido, cuando ocurre el cambio o venta del predio.",
+                            "which": "Se combina con instrumentos de gestión como los planes parciales, las UPR. Es fuente de financiación, dado que el municipio recibe directamente lo recaudado."
+                        },
+                        "multa": {
+                            "name": "Multa",
+                            "definition": "Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad. Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad.",
+                            "requirements": "Requiere tener claras las zonas y los tipos de terrenos: de desarrollo, en la medida que se consoliden, o en renovación, según el POT.",
+                            "when": "Con un POT definido, cuando ocurre el cambio o venta del predio.",
+                            "which": "Se combina con instrumentos de gestión como los planes parciales, las UPR. Es fuente de financiación, dado que el municipio recibe directamente lo recaudado."
+                        },
+                        "peajes": {
+                            "name": "Peajes",
+                            "definition": "Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad. Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad.",
+                            "requirements": "Requiere tener claras las zonas y los tipos de terrenos: de desarrollo, en la medida que se consoliden, o en renovación, según el POT.",
+                            "when": "Con un POT definido, cuando ocurre el cambio o venta del predio.",
+                            "which": "Se combina con instrumentos de gestión como los planes parciales, las UPR. Es fuente de financiación, dado que el municipio recibe directamente lo recaudado."
+                        },
+                        "tarifas": {
+                            "name": "Tarifas",
+                            "definition": "Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad. Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad.",
+                            "requirements": "Requiere tener claras las zonas y los tipos de terrenos: de desarrollo, en la medida que se consoliden, o en renovación, según el POT.",
+                            "when": "Con un POT definido, cuando ocurre el cambio o venta del predio.",
+                            "which": "Se combina con instrumentos de gestión como los planes parciales, las UPR. Es fuente de financiación, dado que el municipio recibe directamente lo recaudado."
+                        },
+                        "licenciaypermisos": {
+                            "name": "Licencia y Permisos",
+                            "definition": "Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad. Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad.",
+                            "requirements": "Requiere tener claras las zonas y los tipos de terrenos: de desarrollo, en la medida que se consoliden, o en renovación, según el POT.",
+                            "when": "Con un POT definido, cuando ocurre el cambio o venta del predio.",
+                            "which": "Se combina con instrumentos de gestión como los planes parciales, las UPR. Es fuente de financiación, dado que el municipio recibe directamente lo recaudado."
+                        },
+                        "derechosdepropiedad": {
+                            "name": "Derechos de Propiedad",
+                            "definition": "Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad. Obliga al pago al municipio por el valor adicional del cambio de suelo que tenga un terreno y se destina de forma directa para obras en la ciudad.",
+                            "requirements": "Requiere tener claras las zonas y los tipos de terrenos: de desarrollo, en la medida que se consoliden, o en renovación, según el POT.",
+                            "when": "Con un POT definido, cuando ocurre el cambio o venta del predio.",
+                            "which": "Se combina con instrumentos de gestión como los planes parciales, las UPR. Es fuente de financiación, dado que el municipio recibe directamente lo recaudado."
+                        }
+                    }
+                }
+
+            }
+
+        }
+    }
+}
+
+$('document').ready(function () {
+    $('.instrument').click(function () {
+        let instrument = $(this).data("instrument");
+        let type = $(this).data("type");
+        let group = $(this).data("group");
+        let name = obj["groups"][group]["types"][type]["instruments"][instrument]["name"];
+        let definition = obj["groups"][group]["types"][type]["instruments"][instrument]["definition"];
+        let requirements = obj["groups"][group]["types"][type]["instruments"][instrument]["requirements"];
+        let when = obj["groups"][group]["types"][type]["instruments"][instrument]["when"];
+        let which = obj["groups"][group]["types"][type]["instruments"][instrument]["which"];
+        toogleTexts(name, definition, requirements, when, which);
+    })
+})
+
+function toogleTexts (name, definition, requirements, when, which) {
+    $('#instrument').html(name);
+    $('#definition').html(definition);
+    $('#requirements').html(requirements);
+    $('#when').html(when);
+    $('#which').html(which);
+}
